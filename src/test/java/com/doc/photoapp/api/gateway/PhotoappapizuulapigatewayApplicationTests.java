b0dc0e473858f7c2bebe5fe1e79f47e98242cbef
anDrekon <4594400@gmail.com>
2021-01-20 12:48:18 +0200

    Initial commit Zuul Api Gateway (balancer)

---
+++ src/test/java/com/doc/photoapp/api/gateway/PhotoappapizuulapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doc.photoapp.api.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappapizuulapigatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
